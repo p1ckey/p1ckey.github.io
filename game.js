9980cb18ee54075cc612766f6e8f015df8ab8819
p1ckey <p1ckey20090618@gmail.com>
2026-06-23 21:04:17 +0900

    623

---
+++ game.js
@@ -0,0 +1,156 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 600;
+
+const keys = {};
+
+document.addEventListener("keydown", (e) => {
+  keys[e.key] = true;
+});
+
+document.addEventListener("keyup", (e) => {
+  keys[e.key] = false;
+});
+
+class Entity {
+  constructor(x, y, img, speed = 4) {
+    this.x = x;
+    this.y = y;
+    this.targetX = this.    x;
+    this.targetY = this.y;
+    this.speed = speed;
+    this.img = img;
+    this.frame = 0;
+    this.anim=0;
+    this.direction = 0;
+    this.moving = false;
+  }
+
+moveTowardTarget() {
+    this.moving = this.x !== this.targetX || this.y !== this.targetY;
+    if (!this.moving) {
+      this.frame = 0;
+      return;
+    }
+    this.anim += 0.05;
+    this.frame = Math.floor(this.anim) % 2;
+
+    const dx = this.targetX - this.x;
+    const dy = this.targetY - this.y;
+    this.x += Math.abs(dx) <= this.speed ? dx : Math.sign(dx) * this.speed;
+    this.y += Math.abs(dy) <= this.speed ? dy : Math.sign(dy) * this.speed;
+  }
+
+  update() {}
+
+  draw(ctx) {
+    const fw = 20;
+    const fh = 20;
+
+    const sx = this.frame * fw;
+    const sy = this.direction * fh;
+
+    ctx.drawImage(this.img, sx, sy, fw, fh, this.x, this.y, fw*4, fh*4);
+  }
+}
+
+class Player extends Entity {
+  update(keys) {
+
+const arrived =
+      this.x === this.targetX &&
+      this.y === this.targetY;
+if(arrived){
+    if (keys["ArrowUp"] || keys["w"]) {
+      this.targetY -= 80;
+      this.direction = 3;
+    }
+
+    else if (keys["ArrowLeft"] || keys["a"]) {
+      this.targetX -= 80;
+      this.direction = 1;
+    }
+
+    else if (keys["ArrowRight"] || keys["d"]) {
+      this.targetX += 80;
+      this.direction = 2;
+    }
+
+    else if (keys["ArrowDown"] || keys["s"]) {
+      this.targetY += 80;
+      this.direction = 0;
+    }
+    }
+// this.moving =
+//       this.x !== this.targetX ||
+//       this.y !== this.targetY;
+
+// if(this.moving){
+// this.anim += 0.1;
+// this.frame = Math.floor(this.anim) % 2;
+
+// if (this.x < this.targetX) this.x += this.speed;
+// if (this.x > this.targetX) this.x -= this.speed;
+
+// if (this.y < this.targetY) this.y += this.speed;
+// if (this.y > this.targetY) this.y -= this.speed;
+
+//   }
+    this.moveTowardTarget();
+}
+}
+const Assets = {
+    player: new Image(),
+    enemy: new Image(),
+    tileset: new Image(),
+};
+
+const tile = {
+    neutral: 0,
+    ally: 1,
+    enemy: 2,
+};
+
+const mapData = [
+    [ 0, 1, 1, 2],
+    [ 0, 1, 1, 2],
+    [ 0, 1, 1, 2],
+    [ 0, 1, 1, 2],
+];
+
+const EnemyTypes = {
+    slime: { speed: 2, baseHp: 10 },
+};
+
+function loadAssets(onReady) {
+  let loaded = 0;
+  const total = 3;
+
+  function check() {
+    loaded++;
+    if (loaded === total) onReady();
+  }
+
+  Assets.player.onload = check;
+  Assets.enemy.onload = check;
+
+  Assets.player.src = "assets/player.png";
+  Assets.enemy.src = "assets/monster1.png";
+  Assets.tileset.src = "assets/tile.png";
+}
+
+function loop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  player.update(keys);
+  player.draw(ctx);
+
+  requestAnimationFrame(loop);
+}
+
+loadAssets(() => {
+  player = new Player(100, 100, Assets.player);
+  loop();
+});
